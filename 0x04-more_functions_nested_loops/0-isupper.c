4d24b80e334e2340f64dc7fb2e2277c2b423107b
liverabel <deeplearnerabel@gmail.com>
2022-03-17 22:35:20 +0300

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,9 @@
-#include "main.h"
-/**
- * _isupper - evalue if letter is uppercase .
- * @c: print int
- * Return: Always 0.
- */
-
+#include <stdio.h>
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-
-else 
-{
-return(0);
-}
-
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
